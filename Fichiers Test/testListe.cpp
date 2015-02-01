5aec4f391918ac553ce97131629206dec331d8ed
gurujam <benais.c@gmail.com>
2015-02-01 15:50:48 +0100

    cell et liste OK

---
+++ Fichiers Test/testListe.cpp
@@ -0,0 +1,21 @@
+#include "cell.h"
+#include "liste.h"
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+	Liste L;
+	Cell a(1);
+	Cell b(2);
+	Cell c(3);
+
+	L.setSuivant(L.getPremier(), &a);
+	L.setSuivant(&a, &b);
+	L.setSuivant(&b, &c);
+	L.affiche();
+
+
+	return 0;
+}
