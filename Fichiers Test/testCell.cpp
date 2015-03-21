761026089e817833f0638a3f26f8d9f91046972e
gurujam <benais.c@gmail.com>
2015-03-21 19:58:23 +0100

    testCell

--- Fichiers Test/testCell.cpp
+++ Fichiers Test/testCell.cpp
@@ -5,26 +5,26 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	Cell c;
+	Cell* c=new Cell(9);
 	Cell cc(10);
 
 	//test constructeurs
-	cout<<"Val c="<<c.getVal()<<endl;
-	cout<<"Val cc="<<cc.getVal()<<endl;
+	cout << "Val c=" << c->getVal() << endl;
+	cout << "Val cc=" << cc.getVal() << endl;
 
 	//test operateur par copie
 	Cell d=cc;
-	cout<<"Val d par copie de cc : "<<d.getVal()<<endl;
+	cout << "Val d par copie de cc : " << d.getVal() << endl;
 
 	//test getter et setter
-	c.setVal(5);
-	cout<<"Nouvelle Val c="<<c.getVal()<<endl;
+	c->setVal(5);
+	cout << "Nouvelle Val c=" << c->getVal() << endl;
 
-	c.setSuiv(&cc);
-	cout<<"Le suivant de c :"<<c.getSuiv()<<endl;
-	cout<<"Verification, &cc="<<&cc<<endl;
+	cc.setSuiv(c);
+	cout << "Le suivant de cc :" << cc.getSuiv() << endl;
+	cout << "Verification : &c=" << c << endl;
 
-
+	delete c;
 
 	return 0;
 }
